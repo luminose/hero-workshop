a7ca94e1d31bec4b9898a57e04a677d1dacc4a87
Joel Bradley <joel dot bradley dot 159 at gmail dot com>
2016-05-19 18:14:17 -0600

    Empty Xcode project

---
+++ Hero Workshop/Hero Workshop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hero Workshop
+//
+//  Created by Joel Bradley on 5/19/16.
+//  Copyright © 2016 Joel Bradley. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
